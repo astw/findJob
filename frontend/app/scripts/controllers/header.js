1bc0c4d9d13ae69b446a9c5b15c4775b64ba7b79
Shuhao Wang <wshuhao@163.com>
2014-11-17 02:01:21 -0800

    fix

--- frontend/app/scripts/controllers/header.js
+++ frontend/app/scripts/controllers/header.js
@@ -3,5 +3,6 @@
 angular.module('psJwtApp')
   .controller('HeaderCtrl', function ($scope, authToken) {
 
-    $scope.isAuthenticated = authToken.isAuthenticated;
+  //  $scope.isAuthenticated = authToken.isAuthenticated;
+        $scope.isAuthenticated =  authToken.isAuthenticated();
   });
